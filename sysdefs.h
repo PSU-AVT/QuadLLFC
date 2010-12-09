6f44745da069f18c7c29d3f1ff19c3e8b891403c
Gregory Haynes <greg@greghaynes.net>
2010-12-09 01:13:59 -0800

    Compiles using lpc1343lib

---
+++ sysdefs.h
@@ -0,0 +1,25 @@
+#ifndef _SYSDEFS_H_
+#define _SYSDEFS_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+// Stay compatible with ugly "windows" style
+#define BOOL bool
+#define TRUE true
+#define FALSE false
+
+typedef volatile uint8_t REG8;
+typedef volatile uint16_t REG16;
+typedef volatile uint32_t REG32;
+typedef unsigned char byte_t;
+
+#define pREG8  (REG8 *)
+#define pREG16 (REG16 *)
+#define pREG32 (REG32 *)
+
+#ifndef NULL
+#define NULL ((void *) 0)
+#endif
+
+#endif
