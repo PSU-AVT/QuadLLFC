a3fa7c922ce66ab271ac56cc605662ae9d1d2177
Gregory Haynes <greg@greghaynes.net>
2011-03-18 20:40:06 -0700

    Created tasks.s with simple tasks system that compiles

---
+++ sys/tasks.c
@@ -0,0 +1,78 @@
+/*
+ * Author: Gregory Haynes <greg@greghaynes.net>
+ *
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2010, Gregory Haynes
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "tasks.h"
+#include "../systick/systick.h"
+
+static struct task_t *tasks;
+static struct task_t *tasks_tail;
+
+void tasks_add_task(struct task_t *task)
+{
+	task->list.next = 0;
+	task->last_exec = 0;
+	if(tasks)
+	{
+		tasks_tail->list.next = &task->list;
+		tasks_tail = task;
+	}
+	else
+	{
+		tasks = task;
+		tasks_tail = task;
+	}
+}
+
+void tasks_loop_forever(void)
+{
+	struct task_t *task_itr;
+	unsigned int cur_ticks;
+
+	while(1) // See function name
+	{
+		task_itr = tasks;
+
+		while(task_itr)
+		{
+			cur_ticks = systickGetTicks();
+
+			if(task_itr->next_exec >= cur_ticks)
+			{
+				// Exec the task
+				task_itr->handler(task_itr);
+			}
+			task_itr->last_exec = cur_ticks;
+			task_itr->next_exec = cur_ticks + task_itr->msecs;
+
+			task_itr = (struct task_t*)task_itr->list.next;
+		}
+	}
+}
---
+++ sys/tasks.h
@@ -0,0 +1,54 @@
+/*
+ * Author: Gregory Haynes <greg@greghaynes.net>
+ *
+ * Software License Agreement (BSD License)
+ *
+ * Copyright (c) 2010, Gregory Haynes
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef TASKS_H
+#define TASKS_H
+
+#include "list.h"
+
+#include <stdint.h>
+
+struct task_t
+{
+	struct list_t list; // Dont touch
+	uint16_t msecs; // How often to exec
+	void (*handler)(struct task_t *task); // Function called every msecs
+	unsigned int last_exec; // Dont touch
+	unsigned int next_exec; // Dont touch
+	void *data; // Set to any data you like
+};
+
+/* Insert tasks in order of priority (greatest first) */
+void tasks_add_task(struct task_t *task);
+
+void tasks_loop_forever(void);
+
+#endif
