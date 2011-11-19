5d4ce5024a2bcf23fadfb31725691631dec5104e
Gregory Haynes <greg@greghaynes.net>
2011-11-19 04:23:06 -0800

    Created state_update_from_gyro

--- state.c
+++ state.c
@@ -1,7 +1,10 @@
 #include "state.h"
 #include "sensors/itg3200.h"
+#include "core/systick.h"
 
 #define STATE_DOF_CNT 6
+
+static float rotation_b_to_i[3][3]; // Body to inertial rotation matrix
 
 void state_add(state_t *s1, state_t *s2, state_t *sum) {
 	float *s1_arr = (float*)s1;
@@ -20,10 +23,14 @@ void state_scale(state_t *s1, float val, state_t *dest) {
 	for(i = 0;i < STATE_DOF_CNT;++i) dest_arr[i] = s1_arr[i] * val;
 }
 
+static uint32_t last_gyro_update_ticks;
+
 void state_update_from_gyro(void) {
 	GyroData gd;
+	uint32_t tick_diff = systickGetTicks() - last_gyro_update_ticks;
+	float dt = tick_diff / 1000;
 	if(itg3200GetData(&gd)) {
-		
+		rotation_matrix_velocity_update(rotation_b_to_i, gd.X, gd.Y, gd.Z, dt);
 	}
 }
 
