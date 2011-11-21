b6bb9da3c4efa1e675e7e99274bb41ac86206729
Gregory Haynes <greg@greghaynes.net>
2011-11-21 00:51:50 -0800

    Created commands.h with commands_handle_message

--- commands.c
+++ commands.c
@@ -1,5 +1,22 @@
 #include "commands.h"
 
+#define COMMAND_HANDLER_MAX 0
+
+typedef void (*command_handler)(unsigned char *buff, uint8_t length);
+
+void commands_set_motor(unsigned char *buff, uint8_t length);
+
+static command_handler command_handlers[] = {
+	commands_set_motor,
+};
+
 void commands_handle_message(unsigned char *buff, uint8_t length) {
+	uint8_t ndx = buff[0];
+	if(ndx > COMMAND_HANDLER_MAX)
+		return;
+	command_handlers[ndx](buff, length);
+}
+
+void commands_set_motor(unsigned char *buff, uint8_t length) {
 }
 
