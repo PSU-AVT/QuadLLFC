b84edd0e3632d42a19a77ff15917d728dec82dfd
Jen Hanni <jeh.wicker@gmail.com>
2012-07-05 14:39:41 -0700

    Removed __new.c after adding all functions to filter.c and updated filter.h to match. Still missing calls and likely have problems with globals. Will fix.

--- filter.c
+++ filter.c
@@ -6,64 +6,105 @@
 #include "commands.h"
 #include "logging.h"
 
-// Sensor fusion using an accelerometer and magenetometer to correct the gyro drift.
+// pull accelerometer data
+// make _state_accel_last.X, etc. available for use and update ticks
 
-void filter_get_accel_data();
-
-void filter_get_mag_data();
-
-void filter_find_total_correction_vector();
-
-void filter_correction_matrix_pi_controller();
-
-void filter_get_and_correct_gyro_data();
-
-/*********************************************************************/
-/* everything below this line is still a mix of __new.c and filter.c */
-
-// using the accelerometer and magnetometer data,
-// calculate the total required correction vector
-
-void find_total_correction_vector() 
+void filter_get_accel_data(float accel_dt) 
 {
-	float corr_vector[3];
+        if((ticks - _state_accel_last_update) >= STATE_ACCEL_UPDATE_INTERVAL) {
 
-	const int weight_rollpitch = 0;
-	float rollpitch_corrplane[3];
-	rollpitch_corrplane = {AccelData.x,AccelData.y,AccelData.z};
+                if(adxl345GetData(&_state_accel_last) == i2c_ok) {
+                        uint32_t accel_tick_diff = systickGetTicks() - _last_accel_update_ticks;
 
-	int weight_yaw;
-	float yaw_corrplane[3];
-	Y_corrplane = {MagData.x,MagData.y,MagData.z};
-
-	int temp = 0;
-	for (i=0; i<3; i++)
-	{
-                temp = weight_rollpitch * rollpitch_corrplane[i];
-		corr_vector[i] = temp + weight_yaw * yaw_corrplane[i];
-	}
+                        // Set dt in seconds
+                        if(_last_accel_update_ticks == 0)
+                                accel_dt = 0;
+                        else
+                                accel_dt = accel_tick_diff / 1000.0;
+         }
 
 }
 
+// pull mag data, which doesn't exist yet
+
+void filter_get_mag_data( float mag_dt)
+{
+        if((ticks - _state_mag_last_update) >= STATE_MAG_UPDATE_INTERVAL) {
+
+                if(hmc5883lGetData(&_state_mag_last) == i2c_ok) {
+                        uint32_t mag_tick_diff = systickGetTicks() - _last_mag_update_ticks;
+
+                        // Set dt in seconds
+                        if(_last_mag_update_ticks == 0)
+                                mag_dt = 0;
+                        else
+                                mag_dt = mag_tick_diff / 1000.0;
+        }
+}
+
+// using existing r_matrix, accel data, magdata -- find total_correction
+
+void filter_find_total_correction_vector() 
+{
+	        void find_total_correction_vector()
+        {
+                const int weight_rollpitch = 0;
+                float rollpitch_corrplane[3];
+                rollpitch_corrplane = {AccelData.X,AccelData.Y,AccelData.Z};
+
+                int weight_yaw = 0;
+                float yaw_corrplane[3];
+                yaw_corrplane = {MagData.X,MagData.Y,MagData.Z};
+
+                int temp = 0;
+                for (i=0; i<3; i++)
+                {
+                        temp = weight_rollpitch * RP_corrplane[i];
+                        _corr_vector[i] = temp + weight_yaw * Y_corrplane[i];
+                }
+        }
+}
+
+// using existing r_matrix, accel data, magdata -- find total_correction
+
+void filter_correction_matrix_pi_controller()
+{
+                const int weight_rollpitch = 0;
+                float rollpitch_corrplane[3];
+                rollpitch_corrplane = {AccelData.X,AccelData.Y,AccelData.Z};
+
+                int weight_yaw = 0;
+                float yaw_corrplane[3];
+                yaw_corrplane = {MagData.X,MagData.Y,MagData.Z};
+
+                int temp = 0;
+                for (i=0; i<3; i++)
+                {
+                        temp = weight_rollpitch * RP_corrplane[i];
+                        _corr_vector[i] = temp + weight_yaw * Y_corrplane[i];
+                }
+}
+
+// using total_correction and available globals, find the error
 // PI controller creates value to correct for the determined error 
 // in the rotation correction matrix
 
-void correction_matrix_pi_controller(const float dt, float corr_vector) {
+void filter_get_gyro_correction_data(float dt)
+{
+        int wI_correction = 1; // what is the value of this? no idea.
+        int kP = 1; // proportional gain constant
+        int kI = 1; // integral gain constant
 
-	int w_correction;
-	int wI_correction; // what to do with this?
-	int kP;
-		int kI;
+        int temp_yaw = 0;
+        int temp_rollpitch = 0;
 
-	w_correction = (kP * corr_vector) + (wI_correction + kI * dt * corr_vector);
+        for (i=0; i<3; i++)
+        {
+                fix_yaw = kP * corr_vector[i]
+                fix_rollpitch = kI * dt * corr_vector[i];
+                fix_rollpitch = temp2 + wI_correction;
+                _gyro_error[i] = fix_yaw + fix_rollpitch;
+        }
 }
 
-// drift_adjustment
-// pass w into rotation_matrix_velocity_update()
-
-void drift_adjustment() {
-	
-// error_correction = gyroscope - adjustment
-
-}
 
--- filter.h
+++ filter.h
@@ -1,10 +1,10 @@
 #ifndef FILTER_H
 #define FILTER_H
 
-void filter_get_accel_data();
-void filter_get_mag_data();
+void filter_get_accel_data(float accel_dt);
+void filter_get_mag_data(float mag_dt);
 void filter_find_total_correction_vector();
 void filter_correction_matrix_pi_controller();
-void filter_get_and_correct_gyro_data();
+void filter_get_gyro_correction_data(float dt);
 
 #endif 
